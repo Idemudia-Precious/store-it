94c376186dbceb6391d2f8b90bfb29ed924c387c
Idemudia-Precious <idemudiapreciousosaze@gmail.com>
2024-11-12 22:19:34 +0100

    Hydration error fixed

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -32,56 +32,56 @@ const Sidebar = ({ fullName, avatar, email }: Props) => {
           height={52}
           className="lg:hidden"
         />
-
-        <nav className="sidebar-nav">
-          <ul className="flex flex-1 flex-col gap-6">
-            {navItems.map(({ url, name, icon }) => (
-              <Link key={name} href={url} className="lg:w-full">
-                <li
-                  className={cn(
-                    "sidebar-nav-item",
-                    pathname === url && "shad-active"
-                  )}
-                >
-                  <Image
-                    src={icon}
-                    alt={name}
-                    width={24}
-                    height={24}
-                    className={cn(
-                      "nav-icon",
-                      pathname === url && "nav-icon-active"
-                    )}
-                  />
-                  <p className="hidden lg:block">{name}</p>
-                </li>
-              </Link>
-            ))}
-          </ul>
-        </nav>
-
-        <Image
-          src="/assets/images/files-2.png"
-          alt="logo"
-          width={506}
-          height={418}
-          className="w-full"
-        />
-
-        <div className="sidebar-user-info">
-          <Image
-            src={avatar}
-            alt="Avatar"
-            width={44}
-            height={44}
-            className="sidebar-user-avatar"
-          />
-          <div className="hidden lg:block">
-            <p className="subtitle-2 capitalize">{fullName}</p>
-            <p className="caption">{email}</p>
-          </div>
-        </div>
       </Link>
+
+      <nav className="sidebar-nav">
+        <ul className="flex flex-1 flex-col gap-6">
+          {navItems.map(({ url, name, icon }) => (
+            <Link key={name} href={url} className="lg:w-full">
+              <li
+                className={cn(
+                  "sidebar-nav-item",
+                  pathname === url && "shad-active"
+                )}
+              >
+                <Image
+                  src={icon}
+                  alt={name}
+                  width={24}
+                  height={24}
+                  className={cn(
+                    "nav-icon",
+                    pathname === url && "nav-icon-active"
+                  )}
+                />
+                <p className="hidden lg:block">{name}</p>
+              </li>
+            </Link>
+          ))}
+        </ul>
+      </nav>
+
+      <Image
+        src="/assets/images/files-2.png"
+        alt="logo"
+        width={506}
+        height={418}
+        className="w-full"
+      />
+
+      <div className="sidebar-user-info">
+        <Image
+          src={avatar}
+          alt="Avatar"
+          width={44}
+          height={44}
+          className="sidebar-user-avatar"
+        />
+        <div className="hidden lg:block">
+          <p className="subtitle-2 capitalize">{fullName}</p>
+          <p className="caption">{email}</p>
+        </div>
+      </div>
     </aside>
   );
 };
